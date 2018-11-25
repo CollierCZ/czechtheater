fe8a2bb156dc28600037bbe06f86d9628815641c
Aaron Collier <aaronc@kentico.com>
2018-11-25 10:05:09 +0100

    Footsie

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Czech Theater, z.s., Tučkova 915/22, Veveří, 602 00 Brno, IČO: 07245904. Theme: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">&copy; Czech Theater, z.s., IČO: 07245904. Theme: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
 
