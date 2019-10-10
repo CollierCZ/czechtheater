b49d9793ec606a631196b96f49fc660ec4200975
Aaron Collier <aaron@collier.cz>
2019-10-10 22:13:18 +0200

    Fix fixes

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,38 +3,38 @@ import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 import "./Gallery.css"
 
-const createImage = (image,index) => {
-  return (
-    <div
-      key={image.url}
-      className="image"
-      onClick={() => this.setState({ showLightbox: true, photoIndex: index })}
-    > 
-      <img 
-          src={image.url + "?w=250&h=250&fit=crop"}
-          alt={image.description}
-      />
-    </div>
-  )
-}
-
 class Gallery extends Component {
   constructor(props) {
-    super(props)
-
+    super(props);
     this.state = {
       photoIndex: 0,
       showLightbox: false,
-    }
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(index) {
+    this.setState({ showLightbox: true, photoIndex: index })
+  }
+  createImageDiv(image,index) {
+    return (
+      <div
+        key={image.url}
+        className="image"
+        onClick={() => this.handleClick(index)}
+      > 
+        <img 
+            src={image.url + "?w=250&h=250&fit=crop"}
+            alt={image.description}
+        />
+      </div>
+    )
   }
   render() {
     const { images  } = this.props;
     const { photoIndex, showLightbox } = this.state
     return (
       <div className={"gallery"}>
-        {images.map((image,index) => {
-          return (createImage(image,index))
-        })}
+        {images.map((image,index) => { createImageDiv(image,index) })}
         {showLightbox && (
           <Lightbox
           mainSrc={images[photoIndex].url}
