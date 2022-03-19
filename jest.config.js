24326266f751a73fa10a4eb329461202705df6d2
Aaron Collier <aaron@collier.cz>
2022-03-19 11:55:08 +0100

    :arrow_up: Upgrade dependencie

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     __PATH_PREFIX__: ``,
   },
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
   },
   snapshotSerializers: [`enzyme-to-json/serializer`],
   setupFiles: [`./src/setUpTests.js`,`<rootDir>/loadershim.js`],
