b738e9ef67dace986e2acbb67ca16367d3887b54
Aaron Collier <aaron@collier.cz>
2024-04-11 21:52:31 +0200

    ✨ Add page listing all facts

---
+++ src/lib/components/Fact.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import RichText from './RichText.svelte';
+	import type { TheaterFact } from '../../kontent-types';
+
+	let {
+		fact
+	}: {
+		fact: TheaterFact;
+	} = $props();
+</script>
+
+<div class="mb-12 grid-cols-3 gap-4 md:grid">
+	{#if fact.elements.image.value[0]}
+		<img
+			class="mb-4"
+			src={`${fact.elements.image.value[0].url}?h=300`}
+			alt={fact.elements.image.value[0].description}
+		/>
+	{/if}
+	<div class="col-span-2">
+		<RichText richTextElement={fact.elements.description} />
+	</div>
+</div>
--- src/routes/fact/+page.svelte
+++ src/routes/fact/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import RichText from '$lib/components/RichText.svelte';
 	import Title from '$lib/components/Title.svelte';
 
 	import { onMount } from 'svelte';
 	import type { TheaterFact } from '../../kontent-types';
+	import Fact from '$lib/components/Fact.svelte';
 
 	export let data: PageData;
 
@@ -23,16 +23,5 @@
 <Title>Czech Theater Fact</Title>
 
 {#if fact}
-	<div class="grid-cols-3 gap-4 md:grid">
-		{#if fact.elements.image.value[0]}
-			<img
-				class="mb-4"
-				src={`${fact.elements.image.value[0].url}?h=300`}
-				alt={fact.elements.image.value[0].description}
-			/>
-		{/if}
-		<div class="col-span-2">
-			<RichText richTextElement={fact.elements.description} />
-		</div>
-	</div>
+	<Fact {fact} />
 {/if}
