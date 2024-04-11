4178401aeb9a1a4c299146a288fe0b0447faeb92
Aaron Collier <aaron@collier.cz>
2024-04-11 21:57:38 +0200

    ⚡️ Switch to better image handling
    For CDN images

--- src/lib/components/Fact.svelte
+++ src/lib/components/Fact.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
 	import RichText from './RichText.svelte';
 	import type { TheaterFact } from '../../kontent-types';
+	import ImageConstrainedHeight from './ImageConstrainedHeight.svelte';
 
 	let {
 		fact
 	}: {
 		fact: TheaterFact;
 	} = $props();
+
+	const factImage = fact.elements.image.value[0];
 </script>
 
 <div class="mb-12 grid-cols-3 gap-4 md:grid">
-	{#if fact.elements.image.value[0]}
-		<img
-			class="mb-4"
-			src={`${fact.elements.image.value[0].url}?h=300`}
-			alt={fact.elements.image.value[0].description}
-		/>
+	{#if factImage}
+		<ImageConstrainedHeight image={factImage} />
 	{/if}
-	<div class="col-span-2">
+	<div class="col-span-2 mt-4 md:mt-0">
 		<RichText richTextElement={fact.elements.description} />
 	</div>
 </div>
