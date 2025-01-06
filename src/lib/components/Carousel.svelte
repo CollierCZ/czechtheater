1f2495aa8d68bcf0ce301968d8ce80540e9d0931
Aaron Collier <aaron@collier.cz>
2025-01-06 20:03:23 +0100

    ♻️ Use included Svelte media queries
    Instead of external package

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import MediaQuery from 'svelte-media-queries';
+  import { MediaQuery } from 'svelte/reactivity';
 
   import type { ElementModels } from '@kontent-ai/delivery-sdk';
   import ImageConstrainedOneDimension from './ImageConstrainedOneDimension.svelte';
@@ -14,6 +14,7 @@
     carouselItems: ElementModels.AssetModel[];
   } = $props();
 
+  // Prepare for slides with different heights
   let currentSlideItem = $state(0);
   let baseImageWidth = $state(826);
 
@@ -49,6 +50,15 @@
 
   const navButtonClasses =
     'flex min-w-16 py-8 sm:py-0 items-center justify-center absolute opacity-60 bg-white z-10 sm:relative top-[calc(50%-44px)] hover:bg-slate-200 focus:bg-slate-200';
+
+  // Set media queries to get sizes for images
+  const xlarge = new MediaQuery('(min-width: 1280px)');
+  const medium = new MediaQuery('(min-width: 768px) and (max-width: 1280px)');
+  const small = new MediaQuery('(min-width: 640px) and (max-width: 768px)');
+  const imageWidth = $derived(
+    xlarge.current ? 826 : medium.current ? 608 : small.current ? 480 : 340
+  );
+  const offset = $derived(currentSlideItem * imageWidth);
 </script>
 
 <section aria-label="gallery-carousel">
@@ -59,44 +69,30 @@
       </button>
     {/if}
     <div class="mx-auto flex overflow-hidden">
-      <MediaQuery
-        query={[
-          '(max-width: 640px',
-          '(min-width: 640px) and (max-width: 768px)',
-          '(min-width: 768px) and (max-width: 1280px)',
-          '(min-width: 1280px)'
-        ]}
-        let:matches
+      <div
+        class="flex max-w-[338px] transition-transform duration-500 motion-reduce:transition-none sm:max-w-[480px] md:max-w-min"
+        style:transform={`translateX(-${offset}px) scaleX(1)`}
+        use:setBaseImageWidth={imageWidth}
+        style:max-height={`${SlideDivHeight}px`}
+        role="group"
       >
-        <!-- eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -->
-        {@const [_base, small, medium, xlarge] = matches}
-        {@const imageWidth = xlarge ? 826 : medium ? 608 : small ? 480 : 340}
-        {@const offset = currentSlideItem * imageWidth}
-        <div
-          class="flex max-w-[338px] transition-transform duration-500 motion-reduce:transition-none sm:max-w-[480px] md:max-w-min"
-          style:transform={`translateX(-${offset}px) scaleX(1)`}
-          use:setBaseImageWidth={imageWidth}
-          style:max-height={`${SlideDivHeight}px`}
-          role="group"
-        >
-          {#each carouselItems as item, itemIndex}
-            {@const imageNumber = itemIndex + 1}
-            {@const hide = itemIndex === currentSlideItem ? false : true}
-            <div
-              class="my-auto min-w-[340px] max-w-[340px] sm:min-w-[480px] md:min-w-[608px] md:max-w-[calc(65ch-8rem)] xl:min-w-[826px] xl:max-w-[826px]"
-              aria-hidden={hide}
-              aria-label={`Image ${imageNumber} of ${carouselItems.length}`}
-              id={`carousel-image-${imageNumber}`}
-            >
-              <ImageConstrainedOneDimension
-                image={item}
-                width={826}
-                priority={itemIndex === 0}
-              />
-            </div>
-          {/each}
-        </div>
-      </MediaQuery>
+        {#each carouselItems as item, itemIndex}
+          {@const imageNumber = itemIndex + 1}
+          {@const hide = itemIndex === currentSlideItem ? false : true}
+          <div
+            class="my-auto min-w-[340px] max-w-[340px] sm:min-w-[480px] md:min-w-[608px] md:max-w-[calc(65ch-8rem)] xl:min-w-[826px] xl:max-w-[826px]"
+            aria-hidden={hide}
+            aria-label={`Image ${imageNumber} of ${carouselItems.length}`}
+            id={`carousel-image-${imageNumber}`}
+          >
+            <ImageConstrainedOneDimension
+              image={item}
+              width={826}
+              priority={itemIndex === 0}
+            />
+          </div>
+        {/each}
+      </div>
     </div>
     {#if carouselItems.length > 1}
       <button onclick={nextItem} class={`${navButtonClasses} right-0`}>
