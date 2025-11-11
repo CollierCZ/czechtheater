a04bf6beb6d018effdde57d15a6d48806b84cc90
Aaron Collier <aaron@collier.cz>
2025-11-11 22:56:26 +0100

    ✨ Add newsletter list page

--- src/routes/newsletter/+page.svelte
+++ src/routes/newsletter/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Heading from '$lib/components/Heading.svelte';
   import RichText from '$lib/components/RichText.svelte';
   import Seo from '$lib/components/Seo.svelte';
   import Title from '$lib/components/Title.svelte';
@@ -17,3 +18,15 @@
 <Title>Behind the Curtain</Title>
 
 <RichText richTextElement={data.newsletterSignUpText} />
+
+<Heading level={2}>See past editions</Heading>
+
+{#each data.newsletterEditions as edition}
+  {@const publishDate = new Date(
+    edition.elements.publish_date.value
+  ).toLocaleString('default', { month: 'long', year: 'numeric' })}
+
+  <p class="font-medium underline hover:no-underline focus:no-underline">
+    <a href={publishDate.toLowerCase().replace(' ', '-')}>{publishDate}</a>
+  </p>
+{/each}
