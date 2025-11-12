e5ac79e2673aeaff9dbf4ed6e04e1e8d757a9bce
Aaron Collier <aaron@collier.cz>
2025-11-12 08:37:42 +0100

    🎨 Prettify

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -7,7 +7,7 @@
     level
   }: {
     children: Snippet;
-    centered?: boolean,
+    centered?: boolean;
     level: 2 | 3 | 4;
   } = $props();
   const levelSize = $derived(level === 2 ? 3 : level === 3 ? 2 : 1);
@@ -15,7 +15,8 @@
 
 <svelte:element
   this={`h${level}`}
-  class="mt-8 pb-4 font-medium text-{levelSize}xl xl:text-{levelSize + 1}xl {centered ? 'text-center' : ''}"
+  class="mt-8 pb-4 font-medium text-{levelSize}xl xl:text-{levelSize +
+    1}xl {centered ? 'text-center' : ''}"
 >
   {@render children()}
 </svelte:element>
